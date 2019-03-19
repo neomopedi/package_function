cd42e9021880b8eed6b69b53bdf9dec55f381195
neomopedi <48671209+neomopedi@users.noreply.github.com>
2019-03-19 12:45:41 +0200

    Update __init__.py

--- package_function/__init__.py
+++ package_function/__init__.py
@@ -1,3 +1,3 @@
 global sum_array, fibonacci, factorial, reverse, bubble_sort, merge_sort, quick_sort
-from packagefunction.recursion import sum_array, fibonacci, factorial, reverse
-from packagefunction.sorting import bubble_sort, merge_sort, quick_sort
+from package_function.recursion import sum_array, fibonacci, factorial, reverse
+from package_function.sorting import bubble_sort, merge_sort, quick_sort
