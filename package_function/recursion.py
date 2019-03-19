c95fed206b78e3f3caba5c6cebed0a28e9532291
neomopedi <48671209+neomopedi@users.noreply.github.com>
2019-03-19 13:02:45 +0200

    Update recursion.py

--- package_function/recursion.py
+++ package_function/recursion.py
@@ -1,17 +1,11 @@
 def sum_array(array):
     return(sum(array))
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> cd42e9021880b8eed6b69b53bdf9dec55f381195
 
 def fibonacci(n):
     if n <= 1:
         return n
     else:
         return fibonacci(n - 1) + fibonacci(n - 2)
-
 
 def factorial(n):
     if n <1:   # base case
@@ -20,16 +14,11 @@ def factorial(n):
         returnNumber = n * factorial( n - 1 )  # recursive call
     print(str(n) + '! = ' + str(returnNumber))
     return returnNumber
-<<<<<<< HEAD
-
 
 def reverse(word):
     return word[::-1]
-    
-=======
-     
+         
 
 def reverse(word):
     return word[::-1]
 
->>>>>>> cd42e9021880b8eed6b69b53bdf9dec55f381195
