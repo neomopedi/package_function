942cd181d17e1fd3f07688b109fc36852b405bba
Neo Mopedi <neomopedi@gmail.com>
2019-03-19 12:37:51 +0200

    update

--- package_function/__init__.py
+++ package_function/__init__.py
@@ -1,2 +1,3 @@
-from package_function.recursion import sum_array, fibonacci, factorial, reverse
-from package_function.sorting import bubble_sort, merge_sort, quick_sort
+global sum_array, fibonacci, factorial, reverse, bubble_sort, merge_sort, quick_sort
+from packagefunction.recursion import sum_array, fibonacci, factorial, reverse
+from packagefunction.sorting import bubble_sort, merge_sort, quick_sort
