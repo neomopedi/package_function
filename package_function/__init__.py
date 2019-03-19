478470b458326fa35739679154ded361d2727b20
neomopedi <48671209+neomopedi@users.noreply.github.com>
2019-03-19 11:18:14 +0200

    Update __init__.py

--- package_function/__init__.py
+++ package_function/__init__.py
@@ -1,2 +1,2 @@
-from . import recursion
-from . import sorting
+from package_function.recursion import sum_array, fibonacci, factorial, reverse
+from package_function.sorting import bubble_sort, merge_sort, quick_sort
