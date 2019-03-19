6165a212c7c533849ed951a4ea0bb7810c49135d
neomopedi <48671209+neomopedi@users.noreply.github.com>
2019-03-19 12:45:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='packagefunction',
+    name='package_function',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/neomopedi/packagefunction.git',
+    url='https://github.com/neomopedi/package_function.git',
     author='<Neo Mopedi>',
     author_email='<neo.mopedi@gmail.com>'
 )
